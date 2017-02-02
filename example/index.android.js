4e1953726384c58f02a5fd42f9bd2ef7f4d01dcd
Travis Nuttall <tuttall@gmail.com>
2017-02-01 21:13:47 -0700

    remove unused imports from example app index files

--- example/index.android.js
+++ example/index.android.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 
--- example/index.ios.js
+++ example/index.ios.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
 import App from './App';
 

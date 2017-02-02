e143c90400d16e5beb887abeb1a4bbb87a319e01
Travis Nuttall <tuttall@gmail.com>
2017-02-01 20:59:33 -0700

    update for react v0.40

--- example/index.ios.js
+++ example/index.ios.js
@@ -2,12 +2,4 @@ import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
 import App from './App';
 
-class UUIDExample extends Component {
-  render() {
-    return (
-      <App />
-    );
-  }
-}
-
-AppRegistry.registerComponent('example', () => UUIDExample);
+AppRegistry.registerComponent('example', () => App);

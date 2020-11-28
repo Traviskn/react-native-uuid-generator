cee8280ff774c8ddac61d5ba46c2c6f0451981a2
piccinnigius <piccinninnigius@gmail.com>
2020-11-28 13:39:51 +0100

    Added support for React Native Web

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import { NativeModules } from 'react-native';
+import { Platform } from 'react-native';
+import { v4 as uuidv4 } from 'uuid';
 
 const { RNUUIDGenerator } = NativeModules;
 
-export default {
-  getRandomUUID(callback) {
-    if (callback) {
-      RNUUIDGenerator.getRandomUUID(callback);
+const web = {
+  pino: 'test',
+  getRandomUUID( callback ) {
+    const uuid = uuidv4().toUpperCase()
+    if ( callback ) {
+      callback( uuid );
     }
     else {
-      return new Promise((resolve, reject) => {
-        RNUUIDGenerator.getRandomUUID(resolve);
-      });
+      return new Promise( ( resolve, reject ) => {
+        resolve( uuid )
+      } );
     }
   }
-};
+}
+
+const native = {
+  getRandomUUID( callback ) {
+    if ( callback ) {
+      RNUUIDGenerator.getRandomUUID( callback );
+    }
+    else {
+      return new Promise( ( resolve, reject ) => {
+        RNUUIDGenerator.getRandomUUID( resolve );
+      } );
+    }
+  }
+}
+
+const toExport = Platform.OS === 'web' ? web : native
+export default toExport;
